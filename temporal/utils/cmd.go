189ec12e31ec123a39197a285bf199fc3715c340
asdozzz <leonid.smirnov@elt-poisk.com>
2024-07-10 22:30:05 +0500

    fic

--- temporal/utils/cmd.go
+++ temporal/utils/cmd.go
@@ -1,42 +1,89 @@
 package utils
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 	"sync"
 )
 
-func ExecCommandNew(workingDirectory string, app []string) (string, error) {
+func ExecCommand(workingDirectory string, app []string) (string, error) {
 	x := app[0]
 	_, app = app[0], app[1:]
 	cmd := exec.Command(x, app...)
 	cmd.Dir = workingDirectory
-	stdout, err := cmd.StdoutPipe()
-	cmd.Stderr = cmd.Stdout
+
+	stdoutIn, err := cmd.StdoutPipe()
+
 	if err != nil {
+		log.Printf("Failed creating command stdoutpipe:  '%s'", err)
 		return "", err
 	}
-	if err = cmd.Start(); err != nil {
+	defer func(stdoutIn io.ReadCloser) {
+		_ = stdoutIn.Close()
+	}(stdoutIn)
+	stdoutScanner := bufio.NewScanner(stdoutIn)
+
+	stderrIn, err := cmd.StderrPipe()
+
+	if err != nil {
+		log.Printf("Failed creating command stderrpipe:  '%s'", err)
 		return "", err
 	}
-	var strs = []string{}
-	for {
-		tmp := make([]byte, 1024)
-		_, err := stdout.Read(tmp)
-		strs = append(strs, string(tmp))
-		if err != nil {
-			break
-		}
+	defer func(stderrIn io.ReadCloser) {
+		_ = stderrIn.Close()
+	}(stderrIn)
+	stderrScanner := bufio.NewScanner(stderrIn)
+
+	err = cmd.Start()
+	if err != nil {
+		log.Printf("cmd.Start() failed with '%s'\n", err)
+		return "", err
 	}
 
-	return strings.Join(strs, "###"), nil
+	var strs = []string{}
+
+	for stdoutScanner.Scan() {
+		// Do something with the line here.
+		strs = append(strs, stdoutScanner.Text())
+		log.Println(stdoutScanner.Text())
+	}
+
+	if stdoutScanner.Err() != nil {
+		_ = cmd.Process.Kill()
+		_ = cmd.Wait()
+		log.Printf("stdoutScanner.Err failed with %s", stdoutScanner.Err())
+		return "", errors.New(fmt.Sprintf("%s", stdoutScanner.Err()))
+	}
+
+	for stderrScanner.Scan() {
+		// Do something with the line here.
+		strs = append(strs, stderrScanner.Text())
+		log.Println(stderrScanner.Text())
+	}
+
+	if stderrScanner.Err() != nil {
+		_ = cmd.Process.Kill()
+		_ = cmd.Wait()
+		log.Printf("stderrScanner.Err failed with %s", stderrScanner.Err())
+		return "", errors.New(fmt.Sprintf("%s", stderrScanner.Err()))
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Printf("cmd.Run() failed with %s", err)
+		return "", errors.New(fmt.Sprintf("%s", err))
+	}
+
+	return strings.Join(strs, "\n"), nil
 }
 
-func ExecCommand(workingDirectory string, app []string) (string, error) {
+func ExecCommandOld(workingDirectory string, app []string) (string, error) {
 	x := app[0]
 	_, app = app[0], app[1:]
 	cmd := exec.Command(x, app...)
@@ -55,11 +102,11 @@ func ExecCommand(workingDirectory string, app []string) (string, error) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		stdout, errStdout = copyAndCaptureNew(stdoutIn)
+		stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn)
 		wg.Done()
 	}()
 
-	stderr, errStderr = copyAndCaptureNew(stderrIn)
+	stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
 
 	wg.Wait()
 
@@ -90,26 +137,6 @@ func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
 			if err != nil {
 				return out, err
 			}
-		}
-		if err != nil {
-			// Read returns io.EOF at the end of file, which is not an error for us
-			if err == io.EOF {
-				err = nil
-			}
-			return out, err
-		}
-	}
-}
-
-func copyAndCaptureNew(r io.Reader) ([]byte, error) {
-	var out []byte
-	buf := make([]byte, 2048, 2048)
-	for {
-		n, err := r.Read(buf[:])
-		if n > 0 {
-			d := buf[:n]
-			out = append(out, d...)
-			return out, nil
 		}
 		if err != nil {
 			// Read returns io.EOF at the end of file, which is not an error for us
