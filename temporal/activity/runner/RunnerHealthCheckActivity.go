8181ec0dc2661eedb550e4c83f33497da1accf35
asdozzzz <asdozzz89@gmail.com>
2024-10-17 23:16:56 +0500

    добавлена activity для очистки podman рудиментов

--- temporal/activity/runner/RunnerHealthCheckActivity.go
+++ temporal/activity/runner/RunnerHealthCheckActivity.go
@@ -2,15 +2,40 @@ package runner
 
 import (
 	"context"
+	"os"
+	"runner/temporal/utils"
 )
 
 type RunnerHealthCheckResult struct {
-	Success bool
+	Success     bool
+	DockerStats string
+	MemInfo     string
 }
 
 func RunnerHealthCheckActivity(ctx context.Context) (*RunnerHealthCheckResult, error) {
 	result := &RunnerHealthCheckResult{
-		Success: true,
+		Success:     true,
+		DockerStats: "",
+		MemInfo:     "",
+	}
+
+	currentPath, err := os.Getwd()
+
+	if err != nil {
+		return result, nil
+	}
+	args := []string{"cat", "/proc/meminfo"}
+	msg, err := utils.ExecCommand(currentPath, args)
+
+	if err == nil {
+		result.MemInfo = msg
+	}
+
+	args = []string{"docker", "stats", "--no-stream", "--format", "json"}
+	msg, err = utils.ExecCommand(currentPath, args)
+
+	if err == nil {
+		result.DockerStats = msg
 	}
 
 	return result, nil
